842016b288e6339b7873eaf1f4c6c1ceaac89fab
yun.park <yun.park@earlysloth.com>
2020-06-13 01:19:40 +0900

    readme 추가

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 import { auth } from "./Auth";
 import { Users } from "../mongo";
 module.exports = (router) => {
-  router.get("/", auth.aa);
+  // router.get("/", auth.aa);
   router.post("/signin", auth.signin);
   router.post("/signup", auth.signup);
   router.post("/duplicateChk", auth.duplicateChk);
