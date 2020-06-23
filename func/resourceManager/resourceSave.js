8429f7f25a3e9a53ff9c6f01867815f0c442389e
Andy0414 <pjh8667@naver.com>
2020-06-23 20:47:07 +0900

    Update resourceManager randomString

--- func/resourceManager/resourceSave.js
+++ func/resourceManager/resourceSave.js
@@ -1,13 +1,20 @@
 import path from "path";
 import { promises as fs } from "fs";
+import crypto from "crypto";
+function makeRandomFileName(type) {
+	let randomStr = crypto.randomBytes(20).toString("hex");
+	console.log(randomStr);
+	return `${randomStr}.${type}`;
+}
 /**
  * @description /public/${filename} 에 file을 저장합니다.
- * @param filename 파일 이름
  * @param file 파일 버퍼
+ * @param type 파일 확장자
  */
-export default async function resourceSave(filename, file) {
+export default async function resourceSave(file, type) {
+	let filename = makeRandomFileName(type);
 	// file path
-	let filepath = path.join("./public", filename);
+	let filepath = path.join("public/", filename);
 	try {
 		// 파일 상태
 		let tFile = await fs.stat(filepath);
@@ -16,8 +23,7 @@ export default async function resourceSave(filename, file) {
 			// 파일 삭제
 			await fs.unlink(filepath);
 		}
-	} catch (err) {
-	}
+	} catch (err) {}
 	try {
 		// 파일 쓰기
 		await fs.writeFile(filepath, file);
