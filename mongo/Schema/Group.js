54ef1c174c80d9770b3ae97b99ba2983c85b8dc1
Andy0414 <pjh8667@naver.com>
2020-06-24 14:13:05 +0900

    Update tabs

--- mongo/Schema/Group.js
+++ mongo/Schema/Group.js
@@ -1,13 +1,13 @@
 import mongoose, { Schema } from "mongoose";
 
 const GroupSchema = new mongoose.Schema({
-	groupName: { type: String },
-	users: [{ type: Schema.Types.ObjectId, ref: "users" }],
-	lat: { type: String },
-	lng: { type: String },
-	time: { type: Date },
-	iconnum: { type: Number },
-	food: { type: String },
+  groupName: { type: String },
+  users: [{ type: Schema.Types.ObjectId, ref: "users" }],
+  lat: { type: String },
+  lng: { type: String },
+  time: { type: Date },
+  iconnum: { type: Number },
+  food: { type: String },
 });
 
 export const Groups = mongoose.model("groups", GroupSchema);
--- routes/Group/index.js
+++ routes/Group/index.js
@@ -2,23 +2,23 @@ import { Groups } from "../../mongo/Schema/Group";
 import { Users } from "../../mongo";
 
 export const group = {
-	createGroup: async (req, res) => {
-		let user = await Users.findOne({ token: req.body.token });
-		if (!user) return res.status(404).json({ message: "token expiration or User Not Found" });
-		else {
-			let data = req.body;
-			let group = new Groups(data);
-			group.users.push(user._id);
-			group = await group.save();
-			return res.status(201).json(group);
-		}
-	},
-	readGroup: async (req, res) => {
-		let user = await Users.findOne({ token: req.body.token });
-		if (!user) return res.status(404).json({ message: "token expiration or User Not Found" });
-		else {
-			let groups = await Groups.find();
-			return res.status(201).json(groups);
-		}
-	},
+  createGroup: async (req, res) => {
+    let user = await Users.findOne({ token: req.body.token });
+    if (!user) return res.status(404).json({ message: "token expiration or User Not Found" });
+    else {
+      let data = req.body;
+      let group = new Groups(data);
+      group.users.push(user._id);
+      group = await group.save();
+      return res.status(201).json(group);
+    }
+  },
+  readGroup: async (req, res) => {
+    let user = await Users.findOne({ token: req.body.token });
+    if (!user) return res.status(404).json({ message: "token expiration or User Not Found" });
+    else {
+      let groups = await Groups.find();
+      return res.status(201).json(groups);
+    }
+  },
 };
