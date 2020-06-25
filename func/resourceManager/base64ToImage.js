c55d3acde7ab7aa39162fe120b78df539e0a8dd8
Andy0414 <pjh8667@naver.com>
2020-06-25 15:00:53 +0900

    UPdate tabs

--- func/resourceManager/base64ToImage.js
+++ func/resourceManager/base64ToImage.js
@@ -4,20 +4,20 @@
  * @returns {imgFile:fileBuffer,imgType:jpeg|png|gif...}
  */
 export default function base64ToImage(base64) {
-	let splitData = base64.split(",");
-	let imgType = null;
-	if (splitData.length > 1) {
-		imgType = splitData[0].split(";")[0].split("/")[1];
-		switch (imgType) {
-			case "jpeg":
-				imgType = "jpg";
-				break;
-		}
-		base64 = splitData[1];
-	}
-	let imageBuffer = Buffer.from(base64, "base64");
-	return {
-		imgFile: imageBuffer,
-		imgType: imgType,
-	};
+  let splitData = base64.split(",");
+  let imgType = null;
+  if (splitData.length > 1) {
+    imgType = splitData[0].split(";")[0].split("/")[1];
+    switch (imgType) {
+      case "jpeg":
+        imgType = "jpg";
+        break;
+    }
+    base64 = splitData[1];
+  }
+  let imageBuffer = Buffer.from(base64, "base64");
+  return {
+    imgFile: imageBuffer,
+    imgType: imgType,
+  };
 }
--- func/resourceManager/resourceSave.js
+++ func/resourceManager/resourceSave.js
@@ -6,24 +6,23 @@ import { promises as fs } from "fs";
  * @param file 파일 버퍼
  */
 export default async function resourceSave(filename, file) {
-	// file path
-	let filepath = path.join("./public", filename);
-	try {
-		// 파일 상태
-		let tFile = await fs.stat(filepath);
-		// 파일이 있으면
-		if (tFile.isFile()) {
-			// 파일 삭제
-			await fs.unlink(filepath);
-		}
-	} catch (err) {
-	}
-	try {
-		// 파일 쓰기
-		await fs.writeFile(filepath, file);
-		// 파일 경로 반환
-		return filename;
-	} catch (err) {
-		throw err;
-	}
+  // file path
+  let filepath = path.join("./public", filename);
+  try {
+    // 파일 상태
+    let tFile = await fs.stat(filepath);
+    // 파일이 있으면
+    if (tFile.isFile()) {
+      // 파일 삭제
+      await fs.unlink(filepath);
+    }
+  } catch (err) {}
+  try {
+    // 파일 쓰기
+    await fs.writeFile(filepath, file);
+    // 파일 경로 반환
+    return filename;
+  } catch (err) {
+    throw err;
+  }
 }
