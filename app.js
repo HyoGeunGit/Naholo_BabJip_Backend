022675505abeccc0778324e23861319fd2ed8763
Andy0414 <pjh8667@naver.com>
2020-06-27 16:19:52 +0900

    Update imgFileBuffer test

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ import cors from "cors";
 import path from "path";
 import moment from "moment";
 import "moment-timezone";
+import base64ToImage from "./func/resourceManager/base64ToImage";
+import resourceSave from "./func/resourceManager/resourceSave";
+import { useStorage } from "./func/firebase/storage";
 
 require("./mongo");
 let app = express();
@@ -21,11 +24,12 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", useRouter);
 
 app.listen(process.env.PORT || 8001, function () {
-  console.log(
-    "Express server listening on port %d in %s mode",
-    this.address().port,
-    app.settings.env
-  );
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
+
+// useStorage.uploadProfile(
+//   "data:image/gif;base64,R0lGODlhDwAOAPYBAAAAAP///+Pj5MzS6qy33bfA4sDI5dne8I6e0JGg0Zim1Jqo1aGu2H6SyZOj0HWMxYWYytbY3V98vLG801p9ulN6u2SHw0JvtYiix8vU4TBmsDBmrx9fqyJgrEB1uGCOxRpeqoGjygdXpwlYpw5aqBVhrGOUx6C+3N/p8+/0+dzd3gBVpQNWpgtdphBgqxpmqxpnqyBqsDB1tmqbx3CfzJe62Z+/3a/K46/J4r/V6c/f7t/q9AJaoQhcpAlcpVuSwh9sqC10rF+Xv5G20cXY5cba5wxnnZu7zXKfthhxmS6EloezvVqcqFeeqJq+w1SjqYC0sajNysrh3drp4trn4fD49O/18vb6+Pf5+OXs6Pz9/Pv8+////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAAAAAAALAAAAAAPAA4AAAeYgAEBThYVFxocJCwrL4KCW0gIDxIUGyAiKys6jlMhCg0TKhgdI5kzjksGDA4RKgInLpkwglhHAwQJEBmCOrErOAFUNQcFCx4uN4IpMis/AUwrJh8lKy45gjvMLQFPmZkymwG9mT4oVU2ZMSmCNt5ARYJaUDwrNgE03kHrjgFRRisxvAnhx09KEm9DCBLMoqQHEYUKrVyBGAgAOw==",
+//   "test2"
+// );
 
 export default app;
--- func/firebase/storage.js
+++ func/firebase/storage.js
@@ -3,10 +3,7 @@ import base64ToImage from "../resourceManager/base64ToImage";
 import rndString from "randomstring";
 const { Storage } = require("@google-cloud/storage");
 const gcs = new Storage({
-  keyFilename: path.join(
-    __dirname,
-    "../../path/gcsKey/Naholo-Babjip-6a0a4fba2081.json"
-  ),
+  keyFilename: path.join(__dirname, "../../path/gcsKey/Naholo-Babjip-6a0a4fba2081.json"),
   projectId: "Nahollo-Babjip",
 });
 
@@ -17,16 +14,15 @@ const config = {
   expires: "03-17-2025",
 };
 const useStorage = {
-  uploadProfile: async (base64) => {
+  uploadProfile: async (base64, fileName) => {
     let img = await base64ToImage(base64);
-    const blob = bucket.file(img.imgFile);
+    const blob = bucket.file(fileName);
     const blobStream = blob.createWriteStream({
       resumable: true,
       metadata: {
         contentType: img.imgType,
       },
     });
-    console.log(img);
     blobStream.on("error", (err) => {
       console.log(err);
       return err;
@@ -34,14 +30,12 @@ const useStorage = {
 
     blobStream.on("finish", () => {
       // The public URL can be used to directly access the file via HTTP.
-      const publicUrl = format(
-        `https://storage.googleapis.com/${bucket.name}/${blob.name}`
-      );
+      const publicUrl = format(`https://storage.googleapis.com/${bucket.name}/${blob.name}`);
       console.log(publicUrl);
       return publicUrl;
     });
 
-    blobStream.end(img);
+    blobStream.end(img.imgFile);
   },
 };
 
