c44a36b81f0bfe9c920e3d41ddea7895dc96365c
yun.park <yun.park@earlysloth.com>
2020-07-04 21:41:51 +0900

    [ADD] socket make Room

--- routes/Matching/index.js
+++ routes/Matching/index.js
@@ -1,12 +1,51 @@
 import rndString from "randomstring";
 module.exports = matching;
+import { admin } from "../../firebase";
+import { Groups, Users } from "../../mongo";
 
-function addGroup(groupUUID, uuid1, uuid2) {}
+let db = admin.database();
+let fireDB = admin.firestore();
+async function addGroup(groupUUID, uuid1, uuid2) {
+  let user1 = await Users.findOne({ uuid: uuid1 });
+  let user2 = await Users.findOne({ uuid: uuid2 });
+  let newGroup = new Groups({
+    groupType: "onebyone",
+    users: [uuid1, uuid2],
+    maximum: 2,
+    groupUUID: groupUUID,
+  });
+
+  try {
+    let addGroup = await newGroup.save();
+    user1.groups.push({ groupUUID, groupType: "onebyone" });
+    user2.groups.push({ groupUUID, groupType: "onebyone" });
+    await user1.save();
+    await user2.save();
+    let dbResult = await db.ref(groupUUID + "/welcome").set({
+      message: "그룹이 생성되었습니다.",
+      timeStamp: "1593480463168",
+      userModel: {
+        name: "ADMIN",
+        photo_profile:
+          "https://cdn.pixabay.com/photo/2016/12/26/17/28/food-1932466_1280.jpg",
+      },
+    });
+    let fbResult = await fireDB
+      .collection("Groups")
+      .doc(groupUUID)
+      .set({
+        users: [fireDB.doc(`/Users/${uuid1}`), fireDB.doc(`/Users/${uuid2}`)],
+      });
+  } catch (e) {
+    console.log("socket ERR!");
+    throw e;
+  }
+}
 function matching(io) {
   let matchingOneToOne = [];
   io.on("connection", (socket) => {
     console.log("new user! : ", socket.id);
-    socket.on("join onetoone", (isVip, sex, uuid) => {
+    socket.on("join onetoone", async (isVip, sex, uuid) => {
       var chk = 1;
       for (var i = 0; i < matchingOneToOne.length; i++) {
         var v = matchingOneToOne[i];
@@ -18,6 +57,7 @@ function matching(io) {
               io.to(v.id);
               io.to(socket.id).emit("matching success", groupUUID);
               chk = false;
+              await addGroup(groupUUID, v.uuid, uuid);
               break;
             }
           } else {
@@ -28,6 +68,7 @@ function matching(io) {
                 io.to(v.id);
                 io.to(socket.id).emit("matching success", groupUUID);
                 chk = false;
+                await addGroup(groupUUID, v.uuid, uuid);
                 break;
               }
             } else {
@@ -36,6 +77,7 @@ function matching(io) {
               io.to(v.id);
               io.to(socket.id).emit("matching success", groupUUID);
               chk = false;
+              await addGroup(groupUUID, v.uuid, uuid);
               break;
             }
           }
